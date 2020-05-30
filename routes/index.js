c89a14272b5db2b9b7c9c97d6c6dbb52d388cb7b
Jose Ruiz Gomez <josruigom88@gmail.com>
2020-05-30 13:11:33 -0700

    Week 1 Assignment

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('/routes/promotionRouter');
+const partnerRouter = require('/routes/partnerRouter');
+
+module.exports = { partnerRouter }
